e683e50cc4b0997cae63225b26979d5f00998df7
WoodsGeo442 <gewoods@csumb.edu>
2020-09-03 15:55:38 -0700

    updated main screen and started log in screen

---
+++ app/src/main/java/edu/csumb/esotorodriguez/project1gradetracker/LogInScreen.java
@@ -0,0 +1,14 @@
+package edu.csumb.esotorodriguez.project1gradetracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInScreen  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_screen);
+    }
+}
